c1d1d2c0c862db50f2a931cd1e4de06bf72c59dc
krup13 <khairanafisa4@gmail.com>
2025-01-12 17:31:05 +0800

    added the basic setup for the bottom navigation bar

--- MindBodyEarth/app/src/main/java/com/example/mindbodyearth/Activities/MainActivity.java
+++ MindBodyEarth/app/src/main/java/com/example/mindbodyearth/Activities/MainActivity.java
@@ -1,27 +1,42 @@
 package com.example.mindbodyearth.Activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import com.example.mindbodyearth.R;
-
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.navigation_dashboard:
+                        selectedFragment = new DashboardFragment();
+                        break;
+                    case R.id.navigation_notifications:
+                        selectedFragment = new NotificationsFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                return true;
+            }
         });
+
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
     }
 }
